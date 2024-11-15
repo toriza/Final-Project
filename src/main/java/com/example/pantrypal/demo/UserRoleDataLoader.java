5b5d468049cb4f16d6dc30f5875a4c81e43a47e2
toriza <zgtori@gmail.com>
2024-11-15 19:15:46 +0100

    Cleaned up the auth, tested

--- src/main/java/com/example/pantrypal/demo/UserRoleDataLoader.java
+++ src/main/java/com/example/pantrypal/demo/UserRoleDataLoader.java
@@ -17,18 +17,14 @@ public class UserRoleDataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        roleService.save(new Role("ROLE_USER"));
         roleService.save(new Role("ROLE_ADMIN"));
+        roleService.save(new Role("ROLE_USER"));
 
-        userService.saveUser(new User("John Doe", "john", "1234"));
-        userService.saveUser(new User("James Smith", "james", "1234"));
-        userService.saveUser(new User("Jane Carry", "jane", "1234"));
-        userService.saveUser(new User("Chris Anderson", "chris", "1234"));
+        userService.saveUser(new User("Admin", "admin", "4321"));
+        userService.saveUser(new User("James", "james", "1234"));
 
-        roleService.addRoleToUser("john", "ROLE_USER");
-        roleService.addRoleToUser("james", "ROLE_ADMIN");
-        roleService.addRoleToUser("jane", "ROLE_USER");
-        roleService.addRoleToUser("chris", "ROLE_ADMIN");
-        roleService.addRoleToUser("chris", "ROLE_USER");
+
+        roleService.addRoleToUser("admin", "ROLE_ADMIN");
+        roleService.addRoleToUser("james", "ROLE_USER");
     }
 }
--- src/main/java/com/example/pantrypal/model/FreshProducts.java
+++ src/main/java/com/example/pantrypal/model/FreshProducts.java
@@ -1,5 +1,6 @@
 package com.example.pantrypal.model;
 
+
 import java.util.Date;
 
 public class FreshProducts extends Product {
